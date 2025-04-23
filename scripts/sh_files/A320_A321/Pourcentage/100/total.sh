bbde0f2afa63899ecf5721013948414b49128979
Arnault Motte <arnault.motte@gmail.com>
2025-04-23 11:00:17 +0200

    A320 A321 test part 2

---
+++ scripts/sh_files/A320_A321/Pourcentage/100/total.sh
@@ -0,0 +1,19 @@
+#creat dir 
+mkdir -p data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321
+mkdir -p data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/100
+mkdir -p data_orly/data_orly/scn/A320_A321/100
+
+#training
+
+uv run python data_orly/scripts/script_train/train_CVAE.py --data data_orly/data/takeoffs_LFPO_07.pkl --weights data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/100/CVAE_both_A320_A321_all.pth --typecodes A320 A321 --num_flights 1.0 1.0 --data_save data_orly/data/sampled_data/combined_data/A320_A321_all.pkl --cuda 0 --scale 1 --balanced 1
+uv run python data_orly/scripts/script_train/train_VAE.py --data data_orly/data/sampled_data/combined_data/A320_A321_all.pkl --weights data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/100/VAE_A321_all.pth --typecodes A321 --cuda 0 --scale 0.9
+uv run python data_orly/scripts/script_train/train_VAE.py --data data_orly/data/sampled_data/combined_data/A320_A321_all.pkl --weights data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/100/VAE_A320.pth --typecodes A320 --cuda 0 --scale 0.9
+
+#scn files
+mkdir -p data_orly/scn/A320_A321/100
+mkdir -p data_orly/src/generation/saved_traff/A320_A321/100
+
+uv run python data_orly/scripts/Test_sim/scn_gen/scn_gen.py --data "data_orly/data/sampled_data/combined_data/A320_A321_all.pkl" --weight_file "data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/100/VAE_A321_all.pth" --nf 2000 --cond 0 --typecodes A321 --scene_file A320_A321/100
+uv run python data_orly/scripts/Test_sim/scn_gen/scn_gen.py --data "data_orly/data/sampled_data/combined_data/A320_A321_all.pkl" --weight_file "data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/100/VAE_A320.pth" --nf 2000 --cond 0 --typecodes A320 --scene_file A320_A321/100
+uv run python data_orly/scripts/Test_sim/scn_gen/scn_gen.py --data "data_orly/data/sampled_data/combined_data/A320_A321_all.pkl" --weight_file "data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/100/CVAE_both_A320_A321_all.pth" --nf 2000 --typecodes A320 A321 --cond 1 --typecode_to_gen A320 --scene_file A320_A321/100
+uv run python data_orly/scripts/Test_sim/scn_gen/scn_gen.py --data "data_orly/data/sampled_data/combined_data/A320_A321_all.pkl" --weight_file "data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/100/CVAE_both_A320_A321_all.pth" --nf 2000 --typecodes A320 A321 --cond 1 --typecode_to_gen A321 --scene_file A320_A321/100
