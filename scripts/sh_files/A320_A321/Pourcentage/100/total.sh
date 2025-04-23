eb4326e3b4f51704c4498388f31593f0aa74813e
arnault <arnault.motte@gmail.com>
2025-04-23 15:14:19 +0200

    save loss every test

--- scripts/sh_files/A320_A321/Pourcentage/100/total.sh
+++ scripts/sh_files/A320_A321/Pourcentage/100/total.sh
@@ -9,6 +9,17 @@ uv run python data_orly/scripts/script_train/train_CVAE.py --data data_orly/data
 uv run python data_orly/scripts/script_train/train_VAE.py --data data_orly/data/sampled_data/combined_data/A320_A321_all.pkl --weights data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/100/VAE_A321_all.pth --typecodes A321 --cuda 0 --scale 0.9
 uv run python data_orly/scripts/script_train/train_VAE.py --data data_orly/data/sampled_data/combined_data/A320_A321_all.pkl --weights data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/100/VAE_A320.pth --typecodes A320 --cuda 0 --scale 0.9
 
+#loss
+mkdir -p data_orly/results/loss/A320_A321
+mkdir -p data_orly/results/loss/A320_A321/100
+
+uv run data_orly/scripts/loss/loss_compute.py --model data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/100/CVAE_both_A320_A321_all.pth --file data_orly/data/takeoffs_LFPO_07.pkl --typecode A320 --typecodes A320 A321 --cond 1 --loss_file data_orly/results/loss/A320_A321/100/loss.csv
+uv run data_orly/scripts/loss/loss_compute.py --model data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/100/CVAE_both_A320_A321_all.pth --file data_orly/data/takeoffs_LFPO_07.pkl --typecode A321 --typecodes A320 A321 --cond 1 --loss_file data_orly/results/loss/A320_A321/100/loss.csv
+uv run data_orly/scripts/loss/loss_compute.py --model data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/100/VAE_A321_all.pth --file data_orly/data/takeoffs_LFPO_07.pkl --typecode A321 --typecodes A320 A321 --cond 0 --loss_file data_orly/results/loss/A320_A321/100/loss.csv
+uv run data_orly/scripts/loss/loss_compute.py --model data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/100/VAE_A320.pth --file data_orly/data/takeoffs_LFPO_07.pkl --typecode A320 --typecodes A320 A321 --cond 0 --loss_file data_orly/results/loss/A320_A321/100/loss.csv
+
+
+
 #scn files
 mkdir -p data_orly/scn/A320_A321/100
 mkdir -p data_orly/src/generation/saved_traff/A320_A321/100
