dd7d81ae6013d694cde6b4ba154fc99772fc8222
Arnault Motte <arnault.motte@gmail.com>
2025-04-25 17:43:24 +0200

    Functionning test

--- scripts/sh_files/A320_A321/Pourcentage/50/total.sh
+++ scripts/sh_files/A320_A321/Pourcentage/50/total.sh
@@ -5,9 +5,9 @@ mkdir -p data_orly/data_orly/scn/A320_A321/50
 
 #training
 
-# uv run python data_orly/scripts/script_train/train_CVAE.py --data data_orly/data/sampled_data/combined_data/A320_A321_all.pkl --weights data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/50/CVAE_both_A320_A321_50.pth --typecodes A320 A321 --num_flights 1.0 0.5 --data_save data_orly/data/sampled_data/combined_data/A320_A321_50.pkl --cuda 1 --scale 0.9 --balanced 0
-# uv run python data_orly/scripts/script_train/train_VAE.py --data data_orly/data/sampled_data/combined_data/A320_A321_50.pkl --weights data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/50/VAE_A321_50.pth --typecodes A321 --cuda 1 --scale 0.5
-#uv run python data_orly/scripts/script_train/train_VAE.py --data data_orly/data/sampled_data/combined_data/A320_A321_all.pkl --weights data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/50/VAE_A320.pth --typecodes A320 --cuda 1 --scale 0.7
+uv run python data_orly/scripts/script_train/train_CVAE.py --data data_orly/data/sampled_data/combined_data/A320_A321_50.pkl --weights data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/50/CVAE_both_A320_A321_50.pth --typecodes A320 A321 --num_flights 1.0  1.0 --data_save data_orly/data/sampled_data/combined_data/A320_A321_50.pkl --cuda 1 --scale 0.7 --balanced 0
+uv run python data_orly/scripts/script_train/train_VAE.py --data data_orly/data/sampled_data/combined_data/A320_A321_50.pkl --weights data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/50/VAE_A321_50.pth --typecodes A321 --cuda 1 --scale 0.4
+# uv run python data_orly/scripts/script_train/train_VAE.py --data data_orly/data/sampled_data/combined_data/A320_A321_all.pkl --weights data_orly/src/generation/models/saved_weights/limited_one_typecode/A320_A321/50/VAE_A320.pth --typecodes A320 --cuda 1 --scale 0.7
 
 
 #loss
